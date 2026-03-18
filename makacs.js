4f3c5528f8c1c2973160af34c1eaf7092cae74c5
3makacs3 <varadixlena@gmail.com>
2026-03-18 18:11:45 +0100

    Update makacs.js

--- makacs.js
+++ makacs.js
@@ -2,14 +2,14 @@ let drawing = []; // tárolja az összes vonalat
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('#c2c8f6'); // világos lila háttér
 }
 
 function draw() {
-  // minden frame-ben újrarajzoljuk az összes vonalat
+  clear(); // átlátszó canvas, háttér a HTML-ben
+
   for (let i = 0; i < drawing.length; i++) {
     let linePoints = drawing[i];
-    stroke('#b46b9f'); // vonal színe
+    stroke('#b46b9f'); // rajzolható vonal színe
     strokeWeight(2);
     noFill();
     beginShape();
@@ -20,7 +20,6 @@ function draw() {
   }
 }
 
-// folyamatos vonal rajzolása egérrel
 function mouseDragged() {
   if (drawing.length == 0 || drawing[drawing.length - 1].finished) {
     drawing.push([]);
@@ -28,7 +27,6 @@ function mouseDragged() {
   drawing[drawing.length - 1].push({x: mouseX, y: mouseY, finished:false});
 }
 
-// vonal lezárása egér felengedésekor
 function mouseReleased() {
   if (drawing.length > 0) {
     drawing[drawing.length - 1].finished = true;
